e624bcae502c56320a91be57564ede27d7c4fd45
devjjongs <whdtjr5689@gmail.com>
2022-04-04 18:17:36 +0900

    week01_daily04
    added practice iteration source code
    added practice Conditional Statement source code

--- week01/src/main/java/com/sparta/week01/prac/Prac.java
+++ week01/src/main/java/com/sparta/week01/prac/Prac.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class Prac {
 
+    /*[1주차]자바 기본 문법 3*/
     /*메소드의 구조*//*
     public 반환타입 메소드명(재료){
         명령 모음
@@ -50,6 +51,35 @@ public class Prac {
     }*/
 
 
+    /*[1]주차 기본 문법 4*/
+    /*"감" 개수 세는 반복문*/
+    public static int countFruit(String fruit) {
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+        fruits.add("딸기");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("딸기");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("감");
+
+        int cnt = 0; // 과일의 개수를 셀 변수
+        for (int i = 0; i < fruits.size(); i++) {
+            String f = fruits.get(i);
+            if (f == fruit) {
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+
+
     public static void main(String[] args) {
 
         /*변수 & 기초 문법*//*
@@ -58,10 +88,9 @@ public class Prac {
          * 변수명은 일반적으로 1)영어, 2)소문자 시작, 3) 구분은 대문자로
          * 자바는 항상 코드를 작성하고 제일 마지막에 세미콜론(;) 사용*/
 
-//        System.out.println("안녕, 스파르타!");
+//      System.out.println("안녕, 스파르타!");
 
         /*[1주차]자바 기본 문법 1*/
-
         /*숫자형 int, float, Long*//*
 
         int a = 7;
@@ -87,7 +116,6 @@ public class Prac {
 
 
         /*[1주차]자바 기본 문법 2*/
-
         /*참거짓 blooean*//*
 
         int myAge = 20;
@@ -144,7 +172,6 @@ public class Prac {
 
 
         /*[1주차]자바 기본 문법 3*/
-
         /*메소드*//*
          * 반복적인 행동을 줄여서 어디든 갖다쓸 수 있는 명령 모음
          * 메소드명은 변수명과 규칙(영어사용, 소문자시작, 공백없음, 대문자구분)이 같음
@@ -189,7 +216,96 @@ public class Prac {
 
 
 
+        /*[1주차]자바 기본 문법 4*/
+        /*반복문*/
+        /*반복문 구조*//*
+          for (시작조건; 반복을계속할조건; 더하기){
+              명령
+          }*/
+        /*과일 목록 준비*//*
+        List<String> fruits = new ArrayList<>();
+        fruits.add("감");
+        fruits.add("배");
+        fruits.add("감");
+        fruits.add("딸기");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("딸기");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("메론");
+        fruits.add("수박");
+        fruits.add("감");
+        System.out.println(fruits);*/
+        /*과일 목록 차례로 인쇄 - 반복문 없이*//*
+        System.out.println(fruits.get(0));
+        System.out.println(fruits.get(1));
+        System.out.println(fruits.get(2));
+        System.out.println(fruits.get(3));
+        System.out.println(fruits.get(4));
+        System.out.println(fruits.get(5));
+        System.out.println(fruits.get(6));
+        System.out.println(fruits.get(7));
+        System.out.println(fruits.get(8));
+        System.out.println(fruits.get(9));
+        System.out.println(fruits.get(10));
+        System.out.println(fruits.get(11));
+        System.out.println(fruits.get(12));*/
+        /*과일 목록 차례로 인쇄 - 반복문 사용*//*
+        for (int i = 0; i < 13; i++) {
+            String fruit = fruits.get(i);
+            System.out.println(fruit);
+        }   // 매번 13개, 14개, 12개.. 개수 세어줘야 할까요?*/
+        /*반복문 연습퀴즈*//*
+        List<String> celebs = new ArrayList<>();
+        celebs.add("아이유");
+        celebs.add("린다G");
+        celebs.add("은비");
+        celebs.add("금비");
+        celebs.add("비");
+        celebs.add("차은우");
+        celebs.add("남주혁");
+        celebs.add("수지");
+        celebs.add("정우성");
+        celebs.add("제니");
+        celebs.add("정국");
 
+        //My code
+        for (int i = 0; i < celebs.size(); i++) {
+            String celeb = celebs.get(i);
+            System.out.println(celeb);
+        }
 
+        System.out.println("=========================");
+
+        //해설
+        for (int idx = 0; idx < celebs.size(); idx++) {
+            System.out.println(celebs.get(idx));
+        }*/
+        /*=======================*/
+        /*조건문*//*
+        boolean 타입을 활용하여 특정 경우일 때 명령을 내리는 문법*/
+        /*미성년자 여부 판별하기*//*
+        int age = 20;
+        if (age > 19) {
+            System.out.println("성인입니다.");
+        } else {
+            System.out.println("미성년자입니다.");
+        }*/
+        /*if문 기본 구조*//*
+        if (조건1) {
+            명령문1    // 조건1이 참일 경우 실행할 명령문1
+        } else {
+            명령문2    //  조건 1이 참이 아닐 경우 실행할 명령문2
+        }*/
+        /*=======================*/
+        /*"감" 개수 세는 반복문*/
+        int gam = countFruit("감");
+        int subak = countFruit("수박");
+        int straw = countFruit("딸기");
+        System.out.println(gam);
+        System.out.println(subak);
+        System.out.println(straw);
     }
 }
